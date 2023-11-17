0de629341148b054a3602c49f92075e5750e9908
s0und0fs1lence <andrei@jfrogmedia.com>
2023-11-17 17:07:27 +0100

    h

--- web/src/auth/LoginPage.js
+++ web/src/auth/LoginPage.js
@@ -1057,7 +1057,7 @@ class LoginPage extends React.Component {
         <div className="login-content" style={{margin: this.props.preview ?? this.parseOffset(application.formOffset)}}>
           {Setting.inIframe() || Setting.isMobile() ? null : <div dangerouslySetInnerHTML={{__html: application.formCss}} />}
           {Setting.inIframe() || !Setting.isMobile() ? null : <div dangerouslySetInnerHTML={{__html: application.formCssMobile}} />}
-          <div className="login-panel" style={{marginLeft : !Setting.isMobile() ? "-100px" : ""}}>
+          <div className="login-panel" style={{marginLeft: !Setting.isMobile() ? "-100px" : ""}}>
             <div className="side-image" style={{display: application.formOffset !== 4 ? "none" : null}}>
               <div dangerouslySetInnerHTML={{__html: application.formSideHtml}} />
             </div>
